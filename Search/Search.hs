33bca2fe59e5905d29247f22a8b4f77fa25b5d27
Nicu Ionita <nicu@ionita.at>
2012-02-02 19:41:48 +0100

    Playing around with the new framework

--- Search/Search.hs
+++ Search/Search.hs
@@ -1,59 +1,90 @@
-module Search where
+module Search.Search where
 
 import Search.TreeState
 
--- First alpha/beta:
+-- First: alpha/beta
 data TreeVars = TreeVars {
          draft :: Int,		-- draft of the search, readonly
-         lscor :: Int,		-- the score of the last node return
-         lpath :: [Move],	-- path of the last node return
          stans :: Int		-- statistic: total node count
      }
 
 data LevelVars = LevelVars {
          level, depth :: Int,	-- level of root is 0; level + depth = draft
+         curmv        :: Move,	-- current move searched at this level
          alpha, beta  :: Int,	-- current alpha & beta
          moves, path  :: [Move]	-- remaining moves and best path
      }
 
-data NodeVars = NodeVars
+data ResVars = ResVars {
+         rscor :: Int,		-- the score of the last node return
+         rpath :: [Move],	-- path of the last node return
+     }
 
+dummyRes = ResVars { rscor = 0, rpath = [] }	-- used when we don't need a result
+
+-- Actually this is search and qsearch in one
 search :: Monad m => Int -> Int -> Int -> m (Int, [Move])
 search alpha beta draft = do
     es <- genEdges	-- generate the root moves
-    let tv = TreeVars  { draft = draft, lscor = 0, lpath = [], stans = 1 }
+    let tv = TreeVars  { draft = draft, stans = 1 }
         lv = LevelVars { level = 0, depth = draft, alpha = alpha, beta = beta, moves = es }
-    traverse tv lv NodeVars firstMove nextMove getRes
+    traverse tv lv ResVars firstMove nextMove getRes
 
-firstMove = do
+-- Called in preparing the first move of the current level
+-- (i.e. the first node of the next level, aka one level deeper)
+firstMove _ = do
     lv <- getLevelState
     case undefined of
-    _ | depth lv == 0   -> qSearch
-      | null (moves lv) -> finalNode
-      | otherwise       -> nextLevel
+    _ | depth lv == 0   -> qSearch	-- the frontier is reached
+      | null (moves lv) -> finalNode	-- we have no valid moves
+      | otherwise       -> nextLevel >>= noRes	-- go deeper
+
+-- Second and further moves at the same level
+nextMove r = do
+    lift undoMove	-- undo the previous move
+    modifyTreeState $ \t -> t { stans = stans t + 1 }	-- one more node after undo move
+    lv <- getLevelState
+    if (rscor r >= beta lv)
+       then betaCut r
+       else do
+          when (rscor r > alpha lv) $
+              modifyLevelState $ \l -> l { alpha = rscor r, path = curmv l : rpath r }
+          case moves lv of
+              [] -> bestPath 
+              _  -> nextLevel
+
+-- When a beta cut occurs, we abort the current level
+-- and return beta (negated) and the refutation path
+betaCut r = do
+  lv <- getLevelState
+  return $ Left r { rscor = - beta lv, rpath = curmv lv : rpath r }
+
+-- When returning from a deeper level, we need to setup the result
+-- to contain the (negated) score and path of that level
+bestPath = do
+    lv <- getLevelState
+    return $ ResVars { rscor = - alpha lv, rpath = path lv }
 
 -- Make move to go to the next level
 -- Here we always have a next move (moves lv /= [])
 nextLevel = do
     lv <- getLevelState
     let (e : es) = moves lv
-        lv' = LevelVars { level = level lv + 1, depth = depth lv - 1,
-                          alpha = - beta lv, beta = - alpha lv, moves = es }
+    putLevelState lv { curmv = e, moves = es }
     lift $ doMove e
-    return $ Just (lv', NodeVars)
+    nes <- if depth lv > 1
+              then lift genMoves	-- next level is at least depth 1
+              else lift genTactMoves	-- next level is depth <= 0
+    let lv' = LevelVars { level = level lv + 1, depth = depth lv - 1, curmv = e,	-- e!!!
+                          alpha = - beta lv, beta = - alpha lv, moves = nes, path = [] }
+    return $ Right lv'
 
--- Second and further moves at the same level
-nextMove = do
-    lift undoMove	-- undo the previous move
-    tv <- getTreeState
-    lv <- getLevelState
-    if (lscor tv >= beta lv)
-       then betaCut
-       else do
-          when (lscor tv > alpha lv) $ do
-              modifyLevelState $ \l -> l { alpha = lscor tv }
-          if null (moves lv)
-             then do
-                 modifyTreeState $ \t -> t { lscor = alpha lv }
-                 return Nothing
-             else nextLevel
+-- As long as the 2 functions (firstMove and nextMove) need to return
+-- different types, we need this helper
+noRes x = case x of
+              Left x'  -> Left x'
+              Right x' -> Right (x', undefined)
+
+finalNode = do
+    v <- lift static	-- the static evaluation
+    return $ Left ResVars { rscor = v, rpath = [] }
--- Search/TreeState.hs
+++ Search/TreeState.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
-module TreeState
+module Search.TreeState
   -- (
   -- DynTree(..),
   -- LevelInitFunc, NodeInitFunc,
@@ -23,7 +23,7 @@ data Adm t l r = Adm {
          trees :: t,	-- tree state
          lstck :: [l],	-- level stack
          lastr :: r	-- last result from an init function
-     }
+     } deriving Show
 
 -- We could use the CC implementation of the state monad to go faster
 type TreeState t l r m = StateT (Adm t l r) m
@@ -31,7 +31,7 @@ type TreeState t l r m = StateT (Adm t l r) m
 getTreeState  = gets trees
 getLevelState = gets lstck >>= return . head
 putTreeState  t = modify $ \s -> s { trees = t }
-putLevelState l = modify $ \s -> s { lstck = l : lstck s }
+putLevelState l = modify $ \s -> s { lstck = l : tail (lstck s) }
 
 modifyTreeState  f = modify $ \s -> s { trees = f $ trees s }
 modifyLevelState f = modify $ \s -> let (l : ls) = lstck s in s { lstck = f l : ls }
@@ -39,16 +39,44 @@ modifyLevelState f = modify $ \s -> let (l : ls) = lstck s in s { lstck = f l : 
 -- These functions are called in preparation of the next level or the next node
 -- Important remarks for semantic:
 -- 1. the function for initialising a new level (and the first node in it) has access
---    (and can modify) the parent level substatus and parend node substatus from the point of
---    view of the new level and node
+--    (and can modify) the parent level substatus from the point of view of the new
+--    level and node
 -- 2. the function for initialising a new (sibling) node has access (and can modify) the
---    current level substatus and the previous sibling node substatus from the point of
---    view of the new node; however, the modification of the previous node status will have
---    effect only during the action itself, because afterwards the node substatus will be
---    replaced by the new created node substatus (or by the parend node, if no new sibling
---    will be created)
+--    current level substatus from the point of view of the new node; however, the
+--    modification of the level status in the last node will have
+--    effect only during the action itself, because afterwards the level substatus will be
+--    lost (by changing back to the lower towards root)
+-- 3. for level 0 there will be no next sibling initialisation call, as root has no sibling
 type LevelInitFunc t l r m = r -> TreeState t l r m (Either r (l, r))
 type NodeInitFunc  t l r m = r -> TreeState t l r m (Either r r)
+
+-- This action goes up to the next level, if any
+up :: Monad m => LevelInitFunc t l r m -> NodeInitFunc t l r m -> TreeState t l r m ()
+up fl fn = do
+  mln <- gets lastr >>= fl	-- new level?
+  case mln of
+    Left r       -> modify (\s -> s { lastr = r })                      >> next fl fn
+    Right (l, r) -> modify (\s -> s { lstck = l : lstck s, lastr = r }) >> up   fl fn
+
+-- This action goes right to the next sibling (if not on level 0)
+next :: Monad m => LevelInitFunc t l n m -> NodeInitFunc t l n m -> TreeState t l n m ()
+next fl fn = do
+  isl0 <- isLevel0
+  if isl0
+     then return ()		-- the tree traversal is ended
+     else do
+       mn <- gets lastr >>= fn	-- next node (sibling)?
+       case mn of
+         Left  r -> modify (\s -> s { lastr = r }) >> down fl fn
+         Right r -> modify (\s -> s { lastr = r }) >> up   fl fn
+
+-- This action goes down one level toward the root
+-- It will never be called in level 0 (see function next)
+down :: Monad m => LevelInitFunc t l r m -> NodeInitFunc t l r m -> TreeState t l r m ()
+down fl fn = modify (\s -> s { lstck = tail $ lstck s }) >> next fl fn
+
+isLevel0 :: Monad m => TreeState t l r m Bool
+isLevel0 = gets lstck >>= return . null
 
 -- A tree traversal takes place in a user monad and is depth first
 -- When entering a tree traversal, we need these parameters:
@@ -70,30 +98,6 @@ traverse t l r fl fn fr = do
   -- extract the results
   fr (trees adm) (head $ lstck adm) (lastr adm)
 
--- This action goes up to the next level, if any
-up :: Monad m => LevelInitFunc t l r m -> NodeInitFunc t l r m -> TreeState t l r m ()
-up fl fn = do
-  mln <- gets lastr >>= fl	-- init new level
-  case mln of
-    Left r       -> modify (\s -> s { lastr = r }) >> next fl fn	-- no new level (leaf) - next node
-    Right (l, r) -> modify (\s -> s { lstck = l : lstck s, lastr = r }) >> up fl fn
-
--- This actions goes right to the next sibling
-next :: Monad m => LevelInitFunc t l n m -> NodeInitFunc t l n m -> TreeState t l n m ()
-next fl fn = do
-  mn <- gets lastr >>= fn	-- next node (sibling)
-  case mn of
-    Left  r -> modify (\s -> s { lastr = r }) >> down fl fn
-    Right r -> modify (\s -> s { lastr = r }) >> up fl fn
-
--- This action goes down one level toward the root
-down :: Monad m => LevelInitFunc t l n m -> NodeInitFunc t l n m -> TreeState t l n m ()
-down fl fn = do
-  ls' <- gets lstck
-  case ls' of
-    []       -> return ()	-- the tree traversal is ended
-    (_ : ls) -> modify (\s -> s { lstck = ls }) >> next fl fn
-
 -- Example and test:
 -- This should (create and) traverse a tree with m levels (from 0 to m-1), with every node in levels 0
 -- to m-2 having n child nodes each (level m-1 contains only leaves), while counting the total number of nodes
@@ -110,24 +114,25 @@ treecount m n = traverse 1 (0, 0) () (treelevel m n) (treenode m n) (\t _ _ -> r
 -- Initialize new level
 -- treelevel :: Monad m => Int -> Int -> TreeState Int (Int, Int) () m (Maybe ((Int, Int), ()))
 treelevel m n _ = do
-  lift $ print $ "treelevel"
   (l, _) <- getLevelState	-- current level
-  if l <= m - 1
-     then return $ Left ()	-- current level is maximum, no further level
+  -- lift $ print $ "treelevel " ++ show l
+  get >>= lift . print
+  if l >= m - 1
+     then return (Left ())	-- current level is maximum, no further level
      else do
-       lift $ print $ "Level " ++ show (l+1) ++ " node 1"
+       -- lift $ print $ "Level " ++ show (l+1) ++ " node 1"
        modifyTreeState $ \t -> t + 1	-- one more node created (the first on the new level)
-       return $ Right ((l+1, n-1), ())	-- next level, n-1 more child at that level
+       return $ Right ((l+1, n-1), ())	-- next level, n-1 more children at that level
 
 -- Initialize new (sibling) node
 -- treenode :: Monad m => Int -> Int -> TreeState Int (Int, Int) () m (Maybe ())
 treenode _ n _ = do
-  lift $ print $ "treenode"
   (l, r) <- getLevelState	-- remaining nodes in current level
+  -- lift $ print $ "treenode " ++ show l ++ " / " ++ show r
   if r <= 0
-     then return $ Left ()	-- no further node here
+     then return (Left ())	-- no further node here
      else do
-       lift $ print $ "Level " ++ show l ++ " node " ++ show (n - r + 1)
+       -- lift $ print $ "Level " ++ show l ++ " node " ++ show (n - r + 1)
        modifyTreeState $ \t -> t + 1		-- one more node here
        putLevelState (l, r-1)	-- level nodes go down
        return $ Right ()
