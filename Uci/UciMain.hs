4e3e9707fb502cdb5bf226b19c59e7b947c7ad78
Nicu Ionita <nicu@ionita.at>
2012-10-14 17:33:07 +0200

    Basis for some speed improvements: speed0

--- Uci/UciMain.hs
+++ Uci/UciMain.hs
@@ -428,7 +428,7 @@ answer s = do
 -- Version and suffix:
 progVersion, progVerSuff, progLogName :: String
 progVersion = "0.61"
-progVerSuff = "rese3"
+progVerSuff = "speed0"
 
 progLogName = "abulafia" ++ "-" ++ progVersion
                  ++ if null progVerSuff then ""
