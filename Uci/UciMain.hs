1bd5d845f698a888986cfc24e37e91bda4b7e6e9
Nicu Ionita <nicu@ionita.at>
2013-03-27 01:52:06 +0100

    Some strictness in Board.hs

--- Uci/UciMain.hs
+++ Uci/UciMain.hs
@@ -435,7 +435,7 @@ answer s = do
 -- Version and suffix:
 progVersion, progVerSuff, progLogName :: String
 progVersion = "0.62"
-progVerSuff = "root"
+progVerSuff = "starg"
 
 progLogName = "abulafia" ++ "-" ++ progVersion
                  ++ if null progVerSuff then ""
